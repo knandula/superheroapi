87c245a2e91eb5d671c208e7133ce8f302f63dbb
Krishna Nandula <knandula@factset.com>
2015-07-29 03:37:22 +0530

    cors

--- api.js
+++ api.js
@@ -34,12 +34,10 @@ var app = express();
 
 app.all('*', function(req, res, next){
     if (!req.get('Origin')) return next();
-    // use "*" here to accept any origin
-
-    res.set('Access-Control-Allow-Origin', 'https://fictiontree.herokuapp.com');
+    res.set('Access-Control-Allow-Origin', '*cd ..');
     res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.set('Access-Control-Allow-Credentials', 'true');
-    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    //res.set('Access-Control-Allow-Credentials', 'true');
+    res.set('Access-Control-Allow-Headers', 'Origin,Accept,Content-Type, Authorization, Content-Length, X-Requested-With');
     // res.set('Access-Control-Allow-Max-Age', 3600);
     if ('OPTIONS' == req.method) return res.send(200);
     next();
